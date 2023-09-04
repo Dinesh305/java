7695f1f43cc2e99b72dd0a457e492b348fc8b415
dinesh-ganesan <dinesh.ganesan@hp.com>
2023-09-04 12:10:05 +0200

    04-09-2023

--- JavaProject/JavaInterviewQuestions/NestedForLoopPatter.java
+++ JavaProject/JavaInterviewQuestions/NestedForLoopPatter.java
@@ -5,9 +5,8 @@ public class NestedForLoopPatter {
 //        pattern2(5);
 //        pattern3(5);
 //        pattern4(5);
-        pattern5(5);
+        pattern5();
     }
-
     static void pattern1(int a) {
         for (int i = 0; i <= a; i++) {
             for (int j = 0; j <= a; j++) {
@@ -16,7 +15,6 @@ public class NestedForLoopPatter {
             System.out.println("");
         }
     }
-
     static void pattern2(int b) {
         for (int i = 0; i <= b; i++) {
             for (int j = 0; j <= i; j++) {
@@ -25,7 +23,6 @@ public class NestedForLoopPatter {
             System.out.println("");
         }
     }
-
     static void pattern3(int c) {
         for (int i = 0; i <= c; i++) {
             for (int j = 5; j >= i; j--) {
@@ -34,23 +31,38 @@ public class NestedForLoopPatter {
             System.out.println("");
         }
     }
-
     static void pattern4(int d) {
         for (int i = 0; i <= d; i++) {
             for (int j = 0; j <= i; j++) {
-
                 System.out.print(j+1);
             }
             System.out.println();
         }
     }
-    static void pattern5(int e) {
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j <= i; j++) {
-
-                System.out.print(j+1);
+    static void pattern5() {
+        for (int i = 0; i <= 4; i++) {
+            int j;
+            for (j = 0; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
+        for (int i = 0; i <= 4; i++) {
+            int j;
+            for (j = 4; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+        static void pattern6 () {
+
+            for (int i = 0; i <= 4; i++) {
+                int j;
+                for (j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
     }
 }
